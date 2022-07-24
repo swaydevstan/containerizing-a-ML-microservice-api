3b196497836f05f408465b102adfa87a5a6484bb
Stanley Godspower <godspowerstanley@gmail.com>
2022-07-24 14:01:38 +0100

    added logger for python

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build -t housing-app:v1 .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -d -p 80:80 housing-app:v1

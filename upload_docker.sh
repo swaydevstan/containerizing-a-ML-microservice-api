1365f8e1f88048b080b0a80fd3f213351565d11c
Stanley Godspower <godspowerstanley@gmail.com>
2022-07-24 14:28:13 +0100

    added upload commands

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="swaydevstan/udacity-housing-app:latest"
 
 # Step 2:  
 # Authenticate & tag
+docker tag housing-app:v1 $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
